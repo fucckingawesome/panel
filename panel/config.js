4a547e283bdd403e4e3b0bd5719cea1ce15020a6
fucckingawesome <godknowspenspinning@gmail.com>
2022-06-14 22:38:02 +0800

    Add files via upload

---
+++ panel/config.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDDb1T_gaUgBfl4lIbHfIO2-70h_CgVcLI",
+  authDomain: "amen-1bc99.firebaseapp.com",
+  projectId: "amen-1bc99",
+  storageBucket: "amen-1bc99.appspot.com",
+  messagingSenderId: "700594999363",
+  appId: "1:700594999363:web:8786a683838ac11fe884f6",
+  measurementId: "G-B7624NJNCP"
+};
+
+    firebase.initializeApp(firebaseConfig); 
+    const db = firebase.firestore();
+
+
+           
